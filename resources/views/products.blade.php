9c4a40000fbaa2a3dabee8d717ebe36f96357de5
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2020-07-09 13:18:22 +0545

    Create products blade for showing products

---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('title', 'Contact Us')
+
+@section('main-section')
+    <div class="inner-register">
+        <div class="overlay-inner">
+            <h3 class="tittle-wthree text-center">Contact Us</h3>
+        </div>
+    </div>
+  
+@endsection
