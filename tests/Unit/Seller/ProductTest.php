26e04b5d166a55bdac7373167a145b85807e95d0
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2020-08-13 16:52:57 +0545

    Seller can add product if logged in.

--- tests/Unit/Seller/ProductTest.php
+++ tests/Unit/Seller/ProductTest.php
@@ -44,6 +44,27 @@ class ProductTest extends TestCase
             ->assertRedirect('/');
     }
 
+    /** @test */
+    public function authenticated_users_can_create_a_new_product()
+    {
+        //Given we have an authenticated user
+        $this->actingAs(factory('App\User')->create(
+            [
+                'isAdmin' => false,
+                'regType' => 'seller',
+            ]
+        ));
+
+        //And a product object
+        $product = factory('App\Product')->create();
+
+        //When user submits post request to create product endpoint
+        $this->from('/addProduct')->post('/addProduct', $product->toArray());
+
+        //It gets stored in the database
+        $this->assertEquals(1,Product::all()->count());
+    }
+
 
 
 
